d0ec4178d59824bdb82f10431976fa37134496dc
xGooddevilx <behroziyan.mhrn@gmail.com>
2025-04-11 18:59:41 +0330

    Introduce react query

---
+++ src/modules/react-query/ReactQueryProvider.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+const makeQueryClient = () => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+};
+
+// eslint-disable-next-line functional/no-let
+let browserQueryClient: QueryClient | undefined;
+
+const getQueryClient = () => {
+  if (isServer) {
+    return makeQueryClient();
+  }
+  if (!browserQueryClient) browserQueryClient = makeQueryClient();
+  return browserQueryClient;
+};
+
+export const ReactQueryProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
