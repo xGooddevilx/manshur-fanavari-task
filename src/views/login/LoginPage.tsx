238bdc07a36111eb02c3ea0f74154542f4c579e6
xGooddevilx <behroziyan.mhrn@gmail.com>
2025-04-13 01:38:33 +0330

    Improve Login page ui

--- src/views/login/LoginPage.tsx
+++ src/views/login/LoginPage.tsx
@@ -1,9 +1,15 @@
-import { LoginForm } from "./components/LoginForm/LoginForm"
+import { LoginForm } from "./components/LoginForm/LoginForm";
 
 export const LoginPage = () => {
   return (
-    <div className="min-w-60 border">
-      <LoginForm/>
+    <div className="min-h-screen w-full bg-gray-100 flex items-center justify-center p-4">
+      <div className="w-full max-w-lg bg-white shadow-xl rounded-2xl p-8 space-y-6">
+        <div className="text-center">
+          <h2 className="text-2xl font-bold">Welcome Back</h2>
+          <p className="text-gray-500 text-sm">Login to your account</p>
+        </div>
+        <LoginForm />
+      </div>
     </div>
-  )
-}
+  );
+};
